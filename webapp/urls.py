eb0cee203d85209f728ac49f2fa8c0aa68261a66
Jaiyl Junushaliev <dzhunushalievzhaiyl@gmail.com>
2024-06-24 14:41:15 +0600

    adding paths
    added path task/
    added path delete/
    added path create_task/

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from webapp.views import index
+from webapp.views import index, detail, delete, create
 
 urlpatterns = [
     path('', index),
+    path('task/', detail),
+    path('delete/', delete),
+    path('create_task/', create)
 ]
